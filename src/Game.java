7fa61402012c293eef5d9189fdb4f04639115120
14nCW <75854264+14nCW@users.noreply.github.com>
2021-06-23 01:24:48 +0200

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.Math;
+
+public class Game {
+    private JButton[] buttons = null;
+
+    public void game(int WIDTH, int HEIGHT, int DIFF) {
+
+        JFrame GameBoard = new JFrame("POWODZENIA!");
+        GameBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GameBoard.setSize(WIDTH, HEIGHT);
+        GameBoard.setVisible(true);
+        JPanel TopPanel = new JPanel();
+        buttons = new JButton[(int) (((WIDTH - 50) * (HEIGHT - 120)) / Math.pow(40, 2))];
+        GameBoard.setLayout(new GridLayout((WIDTH-50)/40,(HEIGHT-120)/40));
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setBackground(Color.BLACK);
+            buttons[i].setVisible(true);
+            GameBoard.add(buttons[i]);
+        }
+
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        JFrame frameMain = new JFrame("Minesweeper");
+        frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameMain.setSize(400, 500);
+
+        JButton buttonEasy = new JButton("ESSA");
+        frameMain.getContentPane().add(buttonEasy);
+        buttonEasy.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frameMain.dispose();
+                game.game(450,620,1);
+            }
+        });
+
+        JButton buttonMedium = new JButton("MED");
+        frameMain.getContentPane().add(buttonEasy);
+        buttonMedium.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frameMain.dispose();
+                game.game(650,920,2);
+            }
+        });
+
+        JButton buttonHard = new JButton("HARD");
+        frameMain.getContentPane().add(buttonEasy);
+        buttonHard.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frameMain.dispose();
+                game.game(1150,920,3);
+            }
+        });
+
+        frameMain.setLayout(new GridLayout(1,3));
+        frameMain.add(buttonEasy);
+        frameMain.add(buttonMedium);
+        frameMain.add(buttonHard);
+        frameMain.setVisible(true);
+        System.out.println("test");
+    }
+}
